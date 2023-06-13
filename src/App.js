6a6b87a405073cd771bbcdb8e4572e7c9371afac
Sataporn Worasilpchai <sworasilpchai@LABA.PSWAS.LOCAL>
2023-06-13 15:55:59 -0700

    prod vs dev

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+// import dotenv from 'dotenv';
+
 function App() {
+
+  const handleclick = ()=>{
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+      // dev code
+      console.log("hello")
+      console.log(process.env.REACT_APP_API_URL_DEVELOPMENT)
+    } else {
+        // production code
+        console.log("world")
+        console.log(process.env.REACT_APP_API_URL_PRODUCTION)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +25,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={handleclick}>Click</button>
         <a
           className="App-link"
           href="https://reactjs.org"
